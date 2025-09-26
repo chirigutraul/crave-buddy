f219efe655f1795a320920d812cb4122993f365f
chirigutraul <chirigutraul@gmail.com>
2025-09-27 00:31:21 +0300

    added design system

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,13 @@ function Home() {
   return (
     <Layout>
       <div>
-        <h1>Home Page</h1>
+        <h1 className="text-accent-1">Home Page</h1>
+        <h2 className="text-accent-2">Home Page</h2>
+        <h3 className="text-primary">Home Page</h3>
+        <h4 className="text-secondary">Home Page</h4>
+        <h5 className="text-accent-1">Home Page</h5>
+        <h6 className="text-accent-2">Home Page</h6>
+        <p className="text-primary">Welcome to the Home Page!</p>
       </div>
     </Layout>
   );
