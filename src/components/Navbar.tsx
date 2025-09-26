17f1afb145b78260d5b41fdd9fcb906c0c653ed4
chirigutraul <chirigutraul@gmail.com>
2025-09-27 00:19:02 +0300

    added layout + empty navbar + hero assets

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  const navLinks = [
+    { label: "Home" },
+    { label: "About" },
+    { label: "Menu" },
+    { label: "Contact" },
+  ];
+
+  return <nav></nav>;
+};
+
+export default Navbar;
---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import Layout from "../layouts/Layout";
+
+function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default Home;
